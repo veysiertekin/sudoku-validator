d8a8bf0111c87610553cb02aec8f4e80c0cdb665
veysiertekin <ertekin.veysi@gmail.com>
2021-05-19 10:30:34 +0100

    Add a null input test

--- src/main/java/org/bitbucket/veysiertekin/sudoku_validator/SudokuValidator.java
+++ src/main/java/org/bitbucket/veysiertekin/sudoku_validator/SudokuValidator.java
@@ -1,4 +1,9 @@
 package org.bitbucket.veysiertekin.sudoku_validator;
 
+import java.util.List;
+
 public class SudokuValidator {
+    public boolean validate(List<Integer> input) {
+        return input != null ;
+    }
 }
--- src/test/java/org/bitbucket/veysiertekin/sudoku_validator/SudokuValidatorTest.java
+++ src/test/java/org/bitbucket/veysiertekin/sudoku_validator/SudokuValidatorTest.java
@@ -1,6 +1,30 @@
 package org.bitbucket.veysiertekin.sudoku_validator;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SudokuValidatorTest {
 
+    @ParameterizedTest
+    @DisplayName("Given input, When it is invalid, Then should return false")
+    @MethodSource("invalidInput")
+    void invalidFormattedInput_ShouldReturnFalse(List<Integer> input, Boolean expectedResult) {
+        boolean result = new SudokuValidator().validate(input);
+        assertThat(result)
+                .isEqualTo(expectedResult);
+    }
+
+    static Stream<Arguments> invalidInput() {
+        return Stream.of(
+                Arguments.of(null, false)
+        );
+    }
 }
