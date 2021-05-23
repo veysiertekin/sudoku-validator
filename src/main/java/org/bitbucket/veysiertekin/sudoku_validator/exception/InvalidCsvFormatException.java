f892bee455f3040004fc796425754471b5668260
veysiertekin <ertekin.veysi@gmail.com>
2021-05-23 05:25:00 +0100

    Fix a typo

--- src/main/java/org/bitbucket/veysiertekin/sudoku_validator/exception/InvalidCsvFormatException.java
+++ src/main/java/org/bitbucket/veysiertekin/sudoku_validator/exception/InvalidCsvFormatException.java
@@ -3,7 +3,7 @@ package org.bitbucket.veysiertekin.sudoku_validator.exception;
 import org.bitbucket.veysiertekin.sudoku_validator.ApplicationMessage;
 
 /**
- * This exception designed to be used for csv file reading and validation stage
+ * This exception designed to be used for csv file reading validation stage
  */
 public class InvalidCsvFormatException extends SudokuRuntimeException {
     public InvalidCsvFormatException(ApplicationMessage message, Object... parameters) {
